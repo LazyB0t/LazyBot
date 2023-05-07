18be0b3c0fd1712d119fd622419a3177ba25872a
Mikhail Lavygin <rillde52@mail.ru>
2023-05-08 06:40:58 +0700

    Added Director and Builder to create Replies

---
+++ src/main/java/ru/berrywoodfamily/ExecutableMessage.java
@@ -0,0 +1,4 @@
+package ru.berrywoodfamily;
+
+public class ExecutableMessage {
+}
---
+++ src/main/java/ru/berrywoodfamily/TelegramReply.java
@@ -0,0 +1,16 @@
+package ru.berrywoodfamily;
+
+import com.pengrad.telegrambot.model.Message;
+
+public class TelegramReply {
+    Reply reply;
+    private Message message;
+
+    public TelegramReply (Reply reply) {
+        this.reply = reply;
+    }
+
+
+
+
+}
