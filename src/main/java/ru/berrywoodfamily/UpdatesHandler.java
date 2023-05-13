5c43454b464a6fb59f8cf8d75b4fa97d7353edd2
Mikhail Lavygin <rillde52@mail.ru>
2023-05-13 21:50:33 +0700

    Modify UpdatesHandler.java methods

--- src/main/java/ru/berrywoodfamily/UpdatesHandler.java
+++ src/main/java/ru/berrywoodfamily/UpdatesHandler.java
@@ -1,58 +1,73 @@
 package ru.berrywoodfamily;
 
 import TGElements.TGMenu;
+import TGElements.TGMessage;
 import XMLElements.*;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.AbstractSendRequest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 
 public class UpdatesHandler {
-private HashMap<Object, Reply> nextReply;
-private Bot bot;
-private DOMBot domBot;
-private Update update;
-public UpdatesHandler(String botPath) {
-    nextReply = new HashMap();
-    domBot = new DOMBot(botPath);
-    bot = domBot.getBot();
-}
-
-public List<AbstractSendRequest> getReplies(Update update) {
-    this.update = update;
-    if(nextReply.containsKey(getChatID())) {
-
-    } else {
-        for(Replies replies: bot.getReplies()) {
-
-        }
+    private HashMap<Object, Reply> nextReply;
+    private Bot bot;
+    private DOMBot domBot;
+    private Update update;
+    public UpdatesHandler(String botPath) {
+        nextReply = new HashMap();
+        domBot = new DOMBot(botPath);
+        bot = domBot.getBot();
     }
-    return null;
-}
 
-private String getUpdateType() {
-    return (update.message() == null) ? "button" : "txt";
-}
-
-private String getUpdateData() {
-    return (update.message() == null) ? update.callbackQuery().data() : update.message().text();
-}
-
-private Object getChatID() {
-   return (update.message() == null) ? update.callbackQuery().message().chat().id() : update.message().chat().id();
-}
-
-private AbstractSendRequest getTGElement(Reply reply) {
-    AbstractSendRequest tgElement;
-    for (String nameChild: reply.getNamesChildren()){
-        switch (nameChild) {
-            case "Menu":
-                //tgElement = new TGMenu(getChatID(),reply.getMenu());
+    public List<AbstractSendRequest> getReplies(Update update) {
+        this.update = update;
+        List<AbstractSendRequest> tgReplies = new ArrayList();
+        if (nextReply.containsKey(getChatID())) {
+            Reply reply = nextReply.get(getChatID());
+            if (reply.getWait().equals(getUpdateType())) {
+                tgReplies.add(getTGElement(nextReply.get(getChatID())));
+            }
+        } else {
+            for (Replies replies: bot.getReplies()) {
+                if (replies.getAfter().equals(getUpdateData())) {
+                    for (Reply reply: replies.getReplies()){
+                        if (!reply.hasAttribute("wait")){
+                            tgReplies.add(getTGElement(reply));
+                        } else {
+                            nextReply.put(getChatID(),reply);
+                            break;
+                        }
+                    }
+                }
+            }
         }
+        return tgReplies;
     }
-    return null;
-}
 
+    private String getUpdateType() {
+        return (update.message() == null) ? "button" : "txt";
+    }
+
+    private String getUpdateData() {
+        return (update.message() == null) ? update.callbackQuery().data() : update.message().text();
+    }
+
+    private Object getChatID() {
+       return (update.message() == null) ? update.callbackQuery().message().chat().id() : update.message().chat().id();
+    }
+
+    private AbstractSendRequest getTGElement(Reply reply) {
+        for (String nameChild: reply.getNamesChildren()){
+            switch (nameChild) {
+                case "Menu":
+                    return new TGMenu(getChatID(),reply.getMenu());
+                case "Message":
+                    return new TGMessage(getChatID(),reply.getMessage());
+            }
+        }
+        return null;
+    }
 }
