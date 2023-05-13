7eaf8027c67a218908c5d44e0ea94a4ebcddbdb0
Mikhail Lavygin <rillde52@mail.ru>
2023-05-13 19:34:31 +0700

    Added the type property to the Request class

--- src/main/java/ru/berrywoodfamily/Request.java
+++ src/main/java/ru/berrywoodfamily/Request.java
@@ -5,10 +5,12 @@ import com.pengrad.telegrambot.model.Update;
 public class Request {
     private Long chatID;
     private String text;
+    private String type;
 
     public Request(Update update){
         chatID = (update.message() == null) ? update.callbackQuery().message().chat().id() : update.message().chat().id();
         text = (update.message() == null) ? update.callbackQuery().data() : update.message().text();
+        type = (update.message() == null) ? "button" : "txt";
     }
 
     public Long getChatID(){
@@ -19,4 +21,7 @@ public class Request {
         return text;
     }
 
+    public String getType() {
+        return type;
+    }
 }
