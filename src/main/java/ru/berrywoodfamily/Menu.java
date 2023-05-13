108408b348b63c6f4335cc74c8ca15abc38822b3
Mikhail Lavygin <rillde52@mail.ru>
2023-05-13 15:30:26 +0700

    Added get methods inside Button.java, Menu.java, Reply.java

--- src/main/java/ru/berrywoodfamily/Menu.java
+++ src/main/java/ru/berrywoodfamily/Menu.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Menu extends AbstractComplexElement {
     private Text text;
     private List<ButtonsArray> buttonsArrays;
+
     public Menu(Node node) {
         super(node);
         buttonsArrays = new ArrayList();
@@ -16,5 +17,13 @@ public class Menu extends AbstractComplexElement {
         for (Node buttonsArray: getElements("ButtonsArray")) {
             buttonsArrays.add(new ButtonsArray(buttonsArray));
         }
+    }
+
+    public Text getText() {
+        return text;
+    }
+
+    public List<ButtonsArray> getButtonsArrays() {
+        return buttonsArrays;
     }
 }
