04e43891ef7df2528c6a228530606a0f80ab8249
Mikhail Lavygin <rillde52@mail.ru>
2023-05-19 05:34:45 +0700

    Implemented abstract listener.
    Added AbstractTGListener class to expand class for custom user tasks and for future overriding in LazyTGListener, transferred and added functionality from UpdatesHandler to this class.
    Added getVariables() method to GetReplies class to get variables from SaveTo class.
    Removed UpdatesHandler class because it is deprecated.

---
+++ src/main/java/AbstractTGListener.java
@@ -0,0 +1,59 @@
+import UpdatesHandling.GetReplies;
+import UpdatesHandling.XMLReplyToTGElem;
+import XMLElements.Bot;
+import XMLElements.DOMBot;
+import XMLElements.Reply;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.AbstractSendRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractTGListener implements UpdatesListener {
+    private Bot bot;
+    private DOMBot domBot;
+    private GetReplies getReplies;
+    private XMLReplyToTGElem xmlAdapter;
+    private TelegramBot tgBotAPI;
+
+    public AbstractTGListener(String botPath, TelegramBot tgBotAPI) {
+        domBot = new DOMBot(botPath);
+        bot = domBot.getBot();
+        getReplies = new GetReplies(bot.getReplies());
+        xmlAdapter = new XMLReplyToTGElem();
+        this.tgBotAPI = tgBotAPI;
+    }
+
+    @Override
+    public int process(List<Update> list) {
+        List<AbstractSendRequest> tgReplies = new ArrayList();
+        for (Update update: list) {
+            for (Reply reply: getReplies.replies(getChatID(update),getUpdateData(update))) {
+                //TODO: Implement a variable intercept using the saveData() method to implement an extension point.
+                saveData(new ArrayList<>(),update);
+                tgReplies.add(xmlAdapter.getTGElem(getChatID(update), reply));
+            }
+        }
+        for (AbstractSendRequest tgReply: getTGReply(tgReplies)){
+            tgBotAPI.execute(tgReply);
+        }
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private Object getChatID(Update update) {
+        return (update.message() == null) ? update.callbackQuery().message().chat().id() : update.message().chat().id();
+    }
+
+    private String getUpdateData(Update update) {
+        return (update.message() == null) ? update.callbackQuery().data() : update.message().text();
+    }
+
+    private String getUpdateType(Update update) {
+        return (update.message() == null) ? "button" : "txt";
+    }
+
+    public abstract void saveData(List<String> values, Update update);
+    public abstract List<AbstractSendRequest> getTGReply(List<AbstractSendRequest> tgReplies);
+}
