5f35427365d63fbfb5cd188603167cd27684c634
Mikhail Lavygin <rillde52@mail.ru>
2023-05-31 07:27:48 +0700

    Added constructor and implementation to AbsIncMessage.
    Added AbsIncMessage(Update update) constructor and getUpdate method implementation.

--- src/main/java/AbsIncMessage.java
+++ src/main/java/AbsIncMessage.java
@@ -1,3 +1,14 @@
+import com.pengrad.telegrambot.model.Update;
+
 public abstract class AbsIncMessage implements IIncMessage {
+    private Update update;
+    public AbsIncMessage(Update update) {
+        this.update = update;
+    }
     public abstract String getData(String dataType);
+
+    @Override
+    public Update getUpdate() {
+        return update;
+    }
 }
