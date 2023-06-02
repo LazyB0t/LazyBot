2fc9e9b32dc6977ab01fe30d26d7322c818adda7
Mikhail Lavygin <rillde52@mail.ru>
2023-06-03 05:59:08 +0700

    Changed the principle of finding the suitable reply.
    I implemented getSuitableReplies method in BaseRepliesManager class.
    I added abstract methods findRepliesByData(BaseIncMessage incMessage) and findRepliesByReply(BaseIncMessage incMessage, Reply reply) to this class.

--- src/main/java/ru/lazybot/BaseRepliesManager.java
+++ src/main/java/ru/lazybot/BaseRepliesManager.java
@@ -8,12 +8,12 @@ import java.util.Map;
 
 public abstract class BaseRepliesManager implements RepliesManager {
     private List<Replies> listReplies;
-    private Map<Object,List<Reply>> nextReplies;
+    private Map<Object,Reply> nextReply;
     private Map<Object,List<Reply>> sentReplies;
 
     public BaseRepliesManager(List<Replies> listReplies) {
         this.listReplies = listReplies;
-        nextReplies = setNextReplies();
+        nextReply = setNextReplies();
         sentReplies = setSentReplies();
     }
 
@@ -21,14 +21,38 @@ public abstract class BaseRepliesManager implements RepliesManager {
         return listReplies;
     }
 
-    public Map<Object,List<Reply>> getNextReplies() {
-        return nextReplies;
+    public Map<Object, Reply> getNextReply() {
+        return nextReply;
     }
 
     public Map<Object, List<Reply>> getSentReplies() {
         return sentReplies;
     }
 
+    @Override
+    public List<Reply> getSuitableReplies(BaseIncMessage incMessage) {
+        if (incMessage.getType().equals("button") && incMessage.getData("callback").equals("Back")) {
+            if (!getSentReplies().isEmpty()) {
+                List<Reply> listSentReplies = getSentReplies().get(incMessage.getChatID());
+                Reply sentReply = listSentReplies.get(listSentReplies.size() - 1);
+                listSentReplies.remove(listSentReplies.size() - 1);
+                return findRepliesByReply(incMessage, sentReply);
+            }
+        } else if (getNextReply().containsKey(incMessage.getChatID())) {
+            Reply nextReply = getNextReply().get(incMessage.getChatID());
+            getNextReply().remove(incMessage.getChatID());
+            return findRepliesByReply(incMessage, nextReply);
+        } else {
+            return findRepliesByData(incMessage);
+        }
+        return null;
+    }
+
     public abstract Map setNextReplies();
+
     public abstract Map setSentReplies();
+
+    public abstract List<Reply> findRepliesByData(BaseIncMessage incMessage);
+
+    public abstract List<Reply> findRepliesByReply(BaseIncMessage incMessage, Reply reply);
 }
