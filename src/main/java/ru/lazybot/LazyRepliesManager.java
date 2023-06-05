b5db2247b2467d10e693e0ffa5ce892bd624c603
Mikhail Lavygin <rillde52@mail.ru>
2023-06-05 10:12:44 +0700

    Reworked the LazyRepliesManager.
    Removed methods from LazyRepliesManager class, which were removed from basic RepliesManager implementation.
    Added setUserReplies method implementation, it returns new object of LazyUserReplies type.

--- src/main/java/ru/lazybot/LazyRepliesManager.java
+++ src/main/java/ru/lazybot/LazyRepliesManager.java
@@ -1,12 +1,8 @@
 package ru.lazybot;
 
 import ru.lazybot.elements.Replies;
-import ru.lazybot.elements.Reply;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LazyRepliesManager extends BaseRepliesManager {
 
@@ -15,53 +11,8 @@ public class LazyRepliesManager extends BaseRepliesManager {
     }
 
     @Override
-    public Map setNextReplies() {
-        return new HashMap();
+    public UserReplies setUserReplies(Replies replies) {
+        return new LazyUserReplies(replies);
     }
 
-    @Override
-    public Map setSentReplies() {
-        return new HashMap();
-    }
-
-    @Override
-    public List<Reply> findRepliesByData(BaseIncMessage incMessage) {
-        return null;
-    }
-
-    @Override
-    public List<Reply> findRepliesByReply(BaseIncMessage incMessage, Reply reply) {
-        return null;
-    }
-
-   /* private List<Reply> findReplies(Object chatID, String after) {
-        List<Reply> relevantReplies = new ArrayList();
-        for (Replies replies : getListReplies()) {
-            if (replies.getAfter().equals(after)) {
-                relevantReplies = replies.getReplies().contains();
-                break;
-            }
-        }
-        for (Reply reply : relevantReplies) {
-            if (reply.hasAttribute("wait")) {
-                getNextReply().put(chatID,relevantReplies.subList(relevantReplies.indexOf(reply),relevantReplies.size()));
-                relevantReplies = relevantReplies.subList(0, relevantReplies.indexOf(reply));
-                break;
-            }
-        }
-        return relevantReplies;
-    }
-
-    private List<Reply> findReplies(Object chatID, List<Reply> replies) {
-        for (Reply reply : replies.subList(1,replies.size())) {
-            if (reply.hasAttribute("wait")) {
-                getNextReply().put(chatID,replies.subList(replies.indexOf(reply),replies.size()));
-                replies = replies.subList(0, replies.indexOf(reply));
-                break;
-            }
-        }
-        return replies;
-    }
-
-    */
 }
