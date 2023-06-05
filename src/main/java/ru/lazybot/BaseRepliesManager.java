5c073291c1912bd403916b3e0cbde8178f6cd9ed
Mikhail Lavygin <rillde52@mail.ru>
2023-06-05 09:55:46 +0700

    The base implementation of RepliesManager has been reworked.
    The BaseRepliesManager class is redesigned so that it no longer monitors and changes the order of the replies in the list, stores UserReplies for a specific user and returns suitable replies.
    Removed nextReply and sentReplies dictionaries for storing next and sent replies.
    Removed getNextReply() and getSentReplies() methods.
    Removed abstract methods setNextReplies(), setSentReplies(), findRepliesByData(BaseIncMessage incMessage) and findRepliesByReply(BaseIncMessage incMessage, Reply reply reply), as their implementation would be unnecessary.
    Added users dictionary to store UserReplies for a specific user.
    Added abstract method setUserReplies(Replies replies): UserReplies.
    Redesigned the getSuitableReplies(BaseIncMessage incMessage) method so that it no longer keeps track of the order of replies for a specific user and does not change it.

--- src/main/java/ru/lazybot/BaseRepliesManager.java
+++ src/main/java/ru/lazybot/BaseRepliesManager.java
@@ -3,56 +3,51 @@ package ru.lazybot;
 import ru.lazybot.elements.Replies;
 import ru.lazybot.elements.Reply;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public abstract class BaseRepliesManager implements RepliesManager {
     private List<Replies> listReplies;
-    private Map<Object,Reply> nextReply;
-    private Map<Object,List<Reply>> sentReplies;
+    private Map<Object, UserReplies> users;
 
     public BaseRepliesManager(List<Replies> listReplies) {
         this.listReplies = listReplies;
-        nextReply = setNextReplies();
-        sentReplies = setSentReplies();
+        users = new HashMap();
     }
 
     public List<Replies> getListReplies() {
         return listReplies;
     }
 
-    public Map<Object, Reply> getNextReply() {
-        return nextReply;
-    }
-
-    public Map<Object, List<Reply>> getSentReplies() {
-        return sentReplies;
-    }
-
     @Override
     public List<Reply> getSuitableReplies(BaseIncMessage incMessage) {
-        if (incMessage.getType().equals("button") && incMessage.getData("callback").equals("Back")) {
-            if (!getSentReplies().isEmpty()) {
-                List<Reply> listSentReplies = getSentReplies().get(incMessage.getChatID());
-                Reply sentReply = listSentReplies.get(listSentReplies.size() - 1);
-                listSentReplies.remove(listSentReplies.size() - 1);
-                return findRepliesByReply(incMessage, sentReply);
+        List<Reply> suitableReplies = new ArrayList();
+        if (users.get(incMessage.getChatID()) != null) {
+            if (incMessage.getType().equals("backButton")) {
+                suitableReplies = users.get(incMessage.getChatID()).getSentReplies();
+                return suitableReplies;
+            } else {
+                suitableReplies = users.get(incMessage.getChatID()).getNextReplies(incMessage);
+                if (suitableReplies != null) {
+                    return suitableReplies;
+                }
             }
-        } else if (getNextReply().containsKey(incMessage.getChatID())) {
-            Reply nextReply = getNextReply().get(incMessage.getChatID());
-            getNextReply().remove(incMessage.getChatID());
-            return findRepliesByReply(incMessage, nextReply);
-        } else {
-            return findRepliesByData(incMessage);
         }
-        return null;
+        for (Replies replies: getListReplies()) {
+            if (replies.getAfter().equals(incMessage.getCommand())) {
+                if (users.get(incMessage.getChatID()) != null) {
+                    users.get(incMessage.getChatID()).addReplies(replies);
+                } else {
+                    users.put(incMessage.getChatID(), setUserReplies(replies));
+                }
+                suitableReplies = users.get(incMessage.getChatID()).getNextReplies(incMessage);
+                break;
+            }
+        }
+        return suitableReplies;
     }
 
-    public abstract Map setNextReplies();
-
-    public abstract Map setSentReplies();
-
-    public abstract List<Reply> findRepliesByData(BaseIncMessage incMessage);
-
-    public abstract List<Reply> findRepliesByReply(BaseIncMessage incMessage, Reply reply);
+    public abstract UserReplies setUserReplies(Replies replies);
 }
