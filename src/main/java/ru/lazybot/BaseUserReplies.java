7c29dc4204570afd000beaeeff33e585571d398e
Mikhail Lavygin <rillde52@mail.ru>
2023-06-05 08:24:13 +0700

    I created a base implementation of UserReplies.
    I created an abstract class BaseUserReplies to store Replies.
    I added BaseUserReplies(Replies) constructor and getReplies() method to it.

---
+++ src/main/java/ru/lazybot/BaseUserReplies.java
@@ -0,0 +1,15 @@
+package ru.lazybot;
+
+import ru.lazybot.elements.Replies;
+
+public abstract class BaseUserReplies implements UserReplies {
+    private Replies replies;
+
+    public BaseUserReplies(Replies replies) {
+        this.replies = replies;
+    }
+
+    public Replies getReplies() {
+        return replies;
+    }
+}
