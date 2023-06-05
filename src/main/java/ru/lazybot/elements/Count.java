34ee9c9786d217d21d481e43052930467d31a50b
Mikhail Lavygin <rillde52@mail.ru>
2023-06-05 13:20:12 +0700

    Added Count mapping.
    Added Count element mapping from xml to the Count class.

---
+++ src/main/java/ru/lazybot/elements/Count.java
@@ -0,0 +1,10 @@
+package ru.lazybot.elements;
+
+import org.jaxptoobjects.AbstractSimpleElement;
+import org.w3c.dom.Node;
+
+public class Count extends AbstractSimpleElement {
+    public Count(Node node) {
+        super(node);
+    }
+}
