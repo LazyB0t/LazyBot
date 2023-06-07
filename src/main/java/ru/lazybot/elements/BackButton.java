24070803b2ab2862a6f5b72a0610d0e70bc91bc6
Mikhail Lavygin <rillde52@mail.ru>
2023-06-08 05:57:52 +0700

    Extended Button.
    Extended the Button class in the BackButton class.
    Changed implementation of the getCallbackData method.

--- src/main/java/ru/lazybot/elements/BackButton.java
+++ src/main/java/ru/lazybot/elements/BackButton.java
@@ -1,23 +1,21 @@
 package ru.lazybot.elements;
 
-import org.jaxptoobjects.AbstractComplexElement;
 import org.w3c.dom.Node;
 
-public class BackButton extends AbstractComplexElement {
+public class BackButton extends Button {
     private Count count;
-    private ButtonLabel buttonLabel;
 
     public BackButton(Node node) {
         super(node);
         count = new Count(getElement("Count"));
-        buttonLabel = new ButtonLabel(getElement("ButtonLabel"));
     }
 
     public Count getCount() {
         return count;
     }
 
-    public ButtonLabel getButtonLabel() {
-        return buttonLabel;
+    @Override
+    public String getCallbackData() {
+        return "/back;" + getCount().getValue();
     }
 }
